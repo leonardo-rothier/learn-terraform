cbc5c43e9010cfb6465be1f22c3e4ca940c1871f
leonardo-rothier <leleorothier@gmail.com>
2025-04-24 23:47:45 +0000

    fix: missing variables type

--- learn-terraform-workspace/variable.tf
+++ learn-terraform-workspace/variable.tf
@@ -1,7 +1,9 @@
 variable "aws_region" {
   description = "All aws resource region"
+  type = string
 }
 
 variable "instance_type" {
   description = "instance type of our ec2 instances"
+  type = string
 }
