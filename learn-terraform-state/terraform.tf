3b0006d099e4c23b56d70fdde2ce0ed5a7551cac
leonardo-rothier <leleorothier@gmail.com>
2025-04-25 17:05:01 +0000

    feat: add state learn terraform configuration

---
+++ learn-terraform-state/terraform.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.95.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.7"
+    }
+  }
+  required_version = "~> 1.11.0"
+}
